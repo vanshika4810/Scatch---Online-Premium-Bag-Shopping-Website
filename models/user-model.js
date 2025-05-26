f2d25118b8a206d2d6ca6cb596af5251022e21a7
vanshika4810 <vanshikalal365@gmail.com>
2025-05-27 00:51:56 +0530

    added separation of concerns in models and created models

---
+++ models/user-model.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+  fullName: {
+    type: String,
+    minLength: 3,
+    trim: true,
+  },
+  email: String,
+  password: String,
+  cart: {
+    type: Array,
+    default: [],
+  },
+  isadmin: Boolean,
+  orders: {
+    type: Array,
+    default: [],
+  },
+  contact: Number,
+  picture: String,
+});
+
+module.exports = mongoose.model("User", userSchema);
