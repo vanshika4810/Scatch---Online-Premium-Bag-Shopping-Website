fd884c6d4ffd3f101e67cacf3c12deda804e306e
vanshika4810 <vanshikalal365@gmail.com>
2025-06-03 21:17:47 +0530

    Add to cart feature added

--- routes/index.js
+++ routes/index.js
@@ -2,15 +2,34 @@ const express = require("express");
 const router = express.Router();
 const isLoggedIn = require("../middlewares/isLoggedIn");
 const productModel = require("../models/product-model");
+const userModel = require("../models/user-model");
 
 router.get("/", (req, res) => {
   let error = req.flash("error");
-  res.render("index", { error });
+  res.render("index", { error, loggedin: false });
 });
 
 router.get("/shop", isLoggedIn, async function (req, res) {
   let products = await productModel.find();
-  res.render("shop", { products });
+  let success = req.flash("success");
+  res.render("shop", { products, success });
+});
+
+router.get("/cart", isLoggedIn, async function (req, res) {
+  let user = await userModel
+    .findOne({ email: req.user.email })
+    .populate("cart");
+
+  const bill = Number(user.cart[0].price) + 20 - Number(user.cart[0].discount);
+  res.render("cart", { user, bill });
+});
+
+router.get("/addtocart/:productid", isLoggedIn, async function (req, res) {
+  let user = await userModel.findOne({ email: req.user.email });
+  user.cart.push(req.params.productid);
+  await user.save();
+  req.flash("success", "Added to Cart");
+  res.redirect("/shop");
 });
 
 router.get("/logout", isLoggedIn, function (req, res) {
